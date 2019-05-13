5b38c9d1ec07a601bb15c0eef294db84ed5b2d99
Giorgi Bagdavadze <notgiorgi@gmail.com>
2019-05-13 14:19:27 +0400

    Updates

---
+++ precache-manifest.f5be646451480e1c8b6a619dc9029595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2285ae5fe8b5fc9941dac06347b1694e",
+    "url": "/virtual-person-taxes/index.html"
+  },
+  {
+    "revision": "9aa5bc1c23207fb0ab05",
+    "url": "/virtual-person-taxes/static/css/main.e96fa48c.chunk.css"
+  },
+  {
+    "revision": "75323096d4907756b130",
+    "url": "/virtual-person-taxes/static/js/2.e72f64c2.chunk.js"
+  },
+  {
+    "revision": "9aa5bc1c23207fb0ab05",
+    "url": "/virtual-person-taxes/static/js/main.7ecc731c.chunk.js"
+  },
+  {
+    "revision": "15d3b3166f1a3210a5fe",
+    "url": "/virtual-person-taxes/static/js/runtime~main.448d44c8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/virtual-person-taxes/precache-manifest.f5be646451480e1c8b6a619dc9029595.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/virtual-person-taxes/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
