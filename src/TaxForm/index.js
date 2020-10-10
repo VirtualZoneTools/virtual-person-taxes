111a469690b8ff1e7b24ebf902078e0464c467fb
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2020-10-10 09:57:06 +0400

    ui/ux tweaks

--- src/TaxForm/index.js
+++ src/TaxForm/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Formik, Field, FieldArray, Form } from 'formik'
 import * as Yup from 'yup'
-import { Button, Pane, Paragraph } from 'evergreen-ui'
+import { Button, Pane, Card, Heading, Paragraph, CrownIcon } from 'evergreen-ui'
 
 import TaxFormInput from './Input'
 import { getReusableData, setReusableData } from '../saveReusableData'
@@ -44,118 +44,133 @@ const Schema = Yup.object().shape({
 
 const TaxForm = ({ onSubmit }) => {
   return (
-    <div>
-      <Formik
-        initialValues={initialState}
-        onSubmit={(values, actions) => {
-          setReusableData(values)
-          onSubmit(values)
-          actions.setSubmitting(false)
-        }}
-        validationSchema={Schema}
-        render={({ errors, status, touched, isSubmitting, isValid, values }) => (
-          <Form>
-            <Field
-              type="text"
-              name="fullName"
-              label="სრული სახელი"
-              component={TaxFormInput}
-              placeholder="მაგ. გიორგი მაისურაძე"
-              required
-            />
+    <Formik
+      initialValues={initialState}
+      onSubmit={(values, actions) => {
+        setReusableData(values)
+        onSubmit(values)
+        actions.setSubmitting(false)
+      }}
+      validationSchema={Schema}
+      render={({ status, isSubmitting, isValid, values }) => (
+        <Form>
+          <Field
+            type="text"
+            name="fullName"
+            label="სრული სახელი"
+            component={TaxFormInput}
+            placeholder="მაგ. გიორგი მაისურაძე"
+            required
+          />
 
-            <Field
-              type="text"
-              name="address"
-              label="მისამართი"
-              component={TaxFormInput}
-              placeholder="მაგ. რუსთაველის გამზ. 26"
-              required
-            />
+          <Field
+            type="text"
+            name="address"
+            label="მისამართი"
+            component={TaxFormInput}
+            placeholder="მაგ. რუსთაველის გამზ. 26"
+            required
+          />
 
-            <Field
-              type="text"
-              name="personalNumber"
-              label="პირადი ნომერი"
-              component={TaxFormInput}
-              placeholder="მაგ. 01101899998"
-              required
-            />
+          <Field
+            type="text"
+            name="personalNumber"
+            label="პირადი ნომერი"
+            component={TaxFormInput}
+            placeholder="მაგ. 01101899998"
+            required
+          />
 
-            <FieldArray
-              name="transactions"
-              render={arrayHelpers => (
-                <Pane>
-                  <Paragraph size={500} marginBottom="15px">
-                    ტრანზაქციები
-                  </Paragraph>
+          <FieldArray
+            name="transactions"
+            render={arrayHelpers => (
+              <Pane>
+                <Heading size={400} marginBottom="15px">
+                  ტრანზაქციები
+                </Heading>
 
-                  {values.transactions &&
-                    values.transactions.length > 0 &&
-                    values.transactions.map((transaction, index) => (
-                      <Pane
-                        key={index}
-                        borderLeft
-                        paddingLeft="15px"
-                        paddingBottom="15px"
-                        marginBottom="15px"
-                      >
-                        <Paragraph size={300} marginBottom="15px">
-                          ტრანზაქცია #{index + 1}
-                        </Paragraph>
+                {values.transactions &&
+                  values.transactions.length > 0 &&
+                  values.transactions.map((transaction, index) => (
+                    <Card
+                      key={index}
+                      elevation={0}
+                      backgroundColor="white"
+                      padding="15px"
+                      marginBottom="15px"
+                    >
+                      <Heading size={300} marginBottom="15px">
+                        ტრანზაქცია #{index + 1}
+                      </Heading>
 
-                        <Field
-                          type="string"
-                          name={`transactions.${index}.date`}
-                          label="თარიღი"
-                          component={TaxFormInput}
-                          placeholder="მაგ. 02/03/2019"
-                          required
-                        />
-                        <Field
-                          type="number"
-                          name={`transactions.${index}.amount`}
-                          label="დივიდენდის რაოდენობა"
-                          component={TaxFormInput}
-                          placeholder="მაგ. 10000"
-                          required
-                        />
+                      <Field
+                        type="string"
+                        name={`transactions.${index}.date`}
+                        label="თარიღი"
+                        component={TaxFormInput}
+                        placeholder="მაგ. 02/03/2019"
+                        required
+                      />
 
-                        {values.transactions.length > 1 && (
-                          <Button
-                            type="button"
-                            appearance="primary"
-                            intent="danger"
-                            iconBefore="remove"
-                            onClick={() => arrayHelpers.remove(index)}
-                          >
-                            ტრანზაქციის წაშლა
-                          </Button>
-                        )}
+                      <Field
+                        type="number"
+                        name={`transactions.${index}.amount`}
+                        label="დივიდენდის რაოდენობა"
+                        component={TaxFormInput}
+                        placeholder="მაგ. 10000"
+                        required
+                      />
+
+                      <Pane display="flex" justifyContent="flex-end">
+                        <Button
+                          type="button"
+                          appearance="minimal"
+                          intent="danger"
+                          iconBefore="remove"
+                          disabled={values.transactions.length === 1}
+                          onClick={() => arrayHelpers.remove(index)}
+                        >
+                          ტრანზაქციის წაშლა
+                        </Button>
                       </Pane>
-                    ))}
+                    </Card>
+                  ))}
+
+                {status && status.msg && <Paragraph>{status.msg}</Paragraph>}
+
+                <Pane
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
                   <Button
                     type="button"
-                    appearance="primary"
+                    appearance="minimal"
                     intent="success"
                     iconBefore="add"
                     onClick={() => arrayHelpers.push('')}
                   >
                     ტრანზაქციის დამატება
                   </Button>
-                </Pane>
-              )}
-            />
 
-            <br />
-            {status && status.msg && <div>{status.msg}</div>}
-            <Button appearance="primary" type="submit" disabled={isSubmitting || !isValid}>
-              ინსტრუქციების დაგენერირება
-            </Button>
-          </Form>
-        )}
-      />
-    </div>
+                  <Button
+                    iconBefore={CrownIcon}
+                    appearance="primary"
+                    height={40}
+                    marginTop="15px"
+                    type="submit"
+                    disabled={isSubmitting || !isValid}
+                  >
+                    ინსტრუქციების დაგენერირება
+                  </Button>
+                </Pane>
+              </Pane>
+            )}
+          />
+        </Form>
+      )}
+    />
   )
 }
 
