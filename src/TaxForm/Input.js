a661453e7270e12ee4cbbd24581b6f35e68720c6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-03-02 13:09:21 +0400

    Some progress upgrading UI for datepicker

--- src/TaxForm/Input.js
+++ src/TaxForm/Input.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import { TextInputField } from 'evergreen-ui'
-import get from 'lodash.get'
+// import get from 'lodash.get'
 
-const TaxFormInput = ({
-  field, // { name, value, onChange, onBlur }
-  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-  ...props
-}) => (
-  <div className="Input-wrapper">
-    <TextInputField
-      marginBottom="15px"
-      type="text"
-      {...field}
-      {...props}
-      validationMessage={get(touched, field.name) && get(errors, field.name)}
-    />
-  </div>
-)
+const TaxFormInput = React.forwardRef((props, ref) => {
+  return (
+    <div className="Input-wrapper">
+      <TextInputField
+        ref={ref}
+        marginBottom="15px"
+        type="text"
+        {...props}
+        // validationMessage={get(touched, field.name) && get(errors, field.name)}
+      />
+    </div>
+  )
+})
 
 export default TaxFormInput
--- src/TaxForm/index.js
+++ src/TaxForm/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { Formik, Field, FieldArray, Form } from 'formik'
+import React, { useRef, useState } from 'react'
+import {
+  Button,
+  Pane,
+  Card,
+  Heading,
+  Paragraph,
+  CrownIcon,
+  DeleteIcon,
+  AddIcon,
+  TrashIcon,
+} from 'evergreen-ui'
 import * as Yup from 'yup'
-import { Button, Pane, Card, Heading, Paragraph, CrownIcon } from 'evergreen-ui'
+import { format, isValid, parse, sub } from 'date-fns'
+import { DayPicker } from 'react-day-picker'
+import { usePopper } from 'react-popper'
+import FocusTrap from 'focus-trap-react'
+
+import 'react-day-picker/dist/style.css'
 
 import TaxFormInput from './Input'
 import { getReusableData, setReusableData } from '../saveReusableData'
+import { useFieldArray, useForm } from 'react-hook-form'
 
 const initialState = {
   ...getReusableData(),
@@ -43,134 +59,207 @@ const Schema = Yup.object().shape({
 })
 
 const TaxForm = ({ onSubmit }) => {
+  const { control, register, handleSubmit } = useForm({
+    defaultValues: initialState,
+  })
+  const { fields: transactions, append, remove } = useFieldArray({
+    control,
+    name: 'transactions',
+  })
+
+  const [selected, setSelected] = useState()
+  const [inputValue, setInputValue] = useState()
+  const [isPopperOpen, setIsPopperOpen] = useState(false)
+
+  const popperRef = useRef()
+  const inputRef = useRef()
+  const [popperElement, setPopperElement] = useState()
+
+  const popper = usePopper(popperRef.current, popperElement, {
+    placement: 'bottom-start',
+  })
+
+  const handleSubmitForm = values => {
+    console.log(values)
+    // TODO: stuff...
+    // setReusableData(values)
+    // onSubmit(values)
+  }
+
+  const handleButtonClick = () => {
+    setIsPopperOpen(true)
+  }
+
+  const handleClosePopper = () => {
+    setIsPopperOpen(false)
+  }
+
+  const handleInputChange = e => {
+    const { value } = e.currentTarget
+    setInputValue(value)
+
+    const date = parse(value, 'y-MM-dd', new Date())
+    if (isValid(date)) {
+      setSelected(date)
+    } else {
+      setSelected(undefined)
+    }
+  }
+
+  const handleDaySelect = date => {
+    setSelected(date)
+    if (date) {
+      setInputValue(format(date, 'y-MM-dd'))
+      handleClosePopper()
+    } else {
+      setInputValue('')
+    }
+  }
+
+  // TODO: implement this.
+  // <Formik
+  //   validationSchema={Schema}
+  //   render={({ status, isSubmitting, isValid, values }) => (
   return (
-    <Formik
-      initialValues={initialState}
-      onSubmit={(values, actions) => {
-        setReusableData(values)
-        onSubmit(values)
-        actions.setSubmitting(false)
-      }}
-      validationSchema={Schema}
-      render={({ status, isSubmitting, isValid, values }) => (
-        <Form>
-          <Field
-            type="text"
-            name="fullName"
-            label="სრული სახელი"
-            component={TaxFormInput}
-            placeholder="მაგ. გიორგი მაისურაძე"
-            required
-          />
+    <form onSubmit={handleSubmit(handleSubmitForm)}>
+      <TaxFormInput
+        type="text"
+        label="სრული სახელი"
+        placeholder="მაგ. გიორგი მაისურაძე"
+        {...register('fullName', { required: true, minLength: 2 })}
+      />
+      <TaxFormInput
+        type="text"
+        label="მისამართი"
+        placeholder="მაგ. რუსთაველის გამზ. 26"
+        {...register('address', { required: true, minLength: 2 })}
+      />
+      <TaxFormInput
+        type="text"
+        label="პირადი ნომერი"
+        placeholder="მაგ. 01101899998"
+        {...register('personalNumber', { required: true, minLength: 11, maxLength: 11 })}
+      />
 
-          <Field
-            type="text"
-            name="address"
-            label="მისამართი"
-            component={TaxFormInput}
-            placeholder="მაგ. რუსთაველის გამზ. 26"
-            required
-          />
+      <Pane>
+        <Heading size={400} marginBottom="15px">
+          ტრანზაქციები
+        </Heading>
 
-          <Field
-            type="text"
-            name="personalNumber"
-            label="პირადი ნომერი"
-            component={TaxFormInput}
-            placeholder="მაგ. 01101899998"
-            required
-          />
+        {transactions.map((transaction, index) => (
+          <Card
+            key={transaction.id}
+            elevation={0}
+            backgroundColor="white"
+            padding="15px"
+            marginBottom="15px"
+            position="relative"
+          >
+            <Pane
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              marginBottom="12px"
+            >
+              <Heading size={400} fontWeight="bold">
+                ტრანზაქცია #{index + 1}
+              </Heading>
 
-          <FieldArray
-            name="transactions"
-            render={arrayHelpers => (
-              <Pane>
-                <Heading size={400} marginBottom="15px">
-                  ტრანზაქციები
-                </Heading>
+              <Button
+                type="button"
+                size="small"
+                intent="danger"
+                iconBefore={TrashIcon}
+                disabled={transactions.length === 1}
+                onClick={() => remove(index)}
+              >
+                წაშლა
+              </Button>
+            </Pane>
 
-                {values.transactions &&
-                  values.transactions.length > 0 &&
-                  values.transactions.map((transaction, index) => (
-                    <Card
-                      key={index}
-                      elevation={0}
-                      backgroundColor="white"
-                      padding="15px"
-                      marginBottom="15px"
-                    >
-                      <Heading size={300} marginBottom="15px">
-                        ტრანზაქცია #{index + 1}
-                      </Heading>
+            <TaxFormInput
+              type="string"
+              label="თარიღი"
+              placeholder="მაგ. 02/03/2019"
+              {...register(`transactions.${index}.date`, { required: true })}
+            />
 
-                      <Field
-                        type="string"
-                        name={`transactions.${index}.date`}
-                        label="თარიღი"
-                        component={TaxFormInput}
-                        placeholder="მაგ. 02/03/2019"
-                        required
-                      />
-
-                      <Field
-                        type="number"
-                        name={`transactions.${index}.amount`}
-                        label="დივიდენდის რაოდენობა"
-                        component={TaxFormInput}
-                        placeholder="მაგ. 10000"
-                        required
-                      />
-
-                      <Pane display="flex" justifyContent="flex-end">
-                        <Button
-                          type="button"
-                          appearance="minimal"
-                          intent="danger"
-                          iconBefore="remove"
-                          disabled={values.transactions.length === 1}
-                          onClick={() => arrayHelpers.remove(index)}
-                        >
-                          ტრანზაქციის წაშლა
-                        </Button>
-                      </Pane>
-                    </Card>
-                  ))}
-
-                {status && status.msg && <Paragraph>{status.msg}</Paragraph>}
-
-                <Pane
-                  display="flex"
-                  flexDirection="column"
-                  justifyContent="space-between"
-                  alignItems="center"
+            {isPopperOpen && (
+              <FocusTrap
+                active
+                focusTrapOptions={{
+                  initialFocus: false,
+                  allowOutsideClick: true,
+                  clickOutsideDeactivates: true,
+                  onDeactivate: handleClosePopper,
+                }}
+              >
+                <div
+                  tabIndex={-1}
+                  style={popper.styles.popper}
+                  className="dialog-sheet"
+                  {...popper.attributes.popper}
+                  ref={setPopperElement}
+                  role="dialog"
                 >
-                  <Button
-                    type="button"
-                    appearance="minimal"
-                    intent="success"
-                    iconBefore="add"
-                    onClick={() => arrayHelpers.push('')}
-                  >
-                    ტრანზაქციის დამატება
-                  </Button>
+                  <DayPicker
+                    initialFocus={isPopperOpen}
+                    mode="single"
+                    defaultMonth={selected}
+                    selected={selected}
+                    onSelect={handleDaySelect}
+                  />
+                </div>
+              </FocusTrap>
+            )}
 
-                  <Button
-                    iconBefore={CrownIcon}
-                    appearance="primary"
-                    height={40}
-                    marginTop="15px"
-                    type="submit"
-                    disabled={isSubmitting || !isValid}
-                  >
-                    ინსტრუქციების დაგენერირება
-                  </Button>
-                </Pane>
+            <TaxFormInput
+              type="number"
+              label="დივიდენდის რაოდენობა"
+              placeholder="მაგ. 10000"
+              {...register(`transactions.${index}.amount`, { required: true })}
+            />
+
+            {index === transactions.length - 1 && (
+              <Pane
+                display="flex"
+                justifyContent="center"
+                position="absolute"
+                left="50%"
+                transform="translateX(-50%)"
+              >
+                <Button
+                  type="button"
+                  intent="success"
+                  appearance="primary"
+                  iconBefore={AddIcon}
+                  onClick={() => append({ date: '', amount: '' })}
+                >
+                  ტრანზაქციის დამატება
+                </Button>
               </Pane>
             )}
-          />
-        </Form>
-      )}
-    />
+          </Card>
+        ))}
+
+        {/* TODO: wsup */}
+        {/* {status && status.msg && <Paragraph>{status.msg}</Paragraph>} */}
+      </Pane>
+
+      <Pane display="flex" justifyContent="center">
+        <Button
+          iconBefore={CrownIcon}
+          appearance="primary"
+          height={40}
+          marginTop="15px"
+          type="submit"
+          // TODO: validation
+          // disabled={isSubmitting || !isValid}
+        >
+          ინსტრუქციების დაგენერირება
+        </Button>
+      </Pane>
+    </form>
   )
 }
 
