8ba18122908c7e4ffc611d2819e5783d0b52c2f6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-03-21 06:15:14 +0400

    improve preview

--- src/Preview/index.tsx
+++ src/Preview/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Input, OrderedList, UnorderedList, Text, Divider, Box } from '@chakra-ui/react'
+import { Heading, Input, OrderedList, UnorderedList, Text, Link } from '@chakra-ui/react'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
@@ -20,12 +20,23 @@ const MarkdownPreview: React.VFC<MarkdownPreviewProps> = ({ data }) => {
         h4: (props) => <Heading as="h4" size="lg" {...props} />,
         h5: (props) => <Heading as="h5" size="md" {...props} />,
         h6: (props) => <Heading as="h6" size="xs" {...props} />,
-        ul: UnorderedList,
-        ol: OrderedList,
+        ul: (props) => <UnorderedList {...props} />,
+        ol: (props) => <OrderedList {...props} />,
         p: (props) => <Text as="p" {...props} />,
         strong: (props) => <Text as="strong" {...props} fontWeight="bold" />,
         em: (props) => <Text as="em" {...props} fontStyle="italic" />,
-        input: (props) => <Input {...props} size="xs" fontWeight="bold" width="min" />,
+        input: (props) => (
+          <Input
+            {...props}
+            value={undefined}
+            defaultValue={props.value}
+            size="xs"
+            fontWeight="bold"
+            textColor="blue.600"
+            width="min"
+          />
+        ),
+        a: (props) => <Link as="a" {...props} color="blue.600" fontWeight="bold" isExternal />,
       }}
       children={generateStepsMD(data)}
       skipHtml={false}
