3c7ac5e6788236458b30253342a23c3cca600752
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-07-18 22:43:29 +0400

    ugly overrides

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,49 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import {
+  extendTheme,
+  useColorModeValue,
+  type ThemeConfig,
+  type ComponentStyleConfig,
+} from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: true,
 }
 
-const theme = extendTheme({
+const GlobalStyle = {
+  'html, body': {
+    // backgroundColor: (...props: any) => ,
+  },
+  '.chakra-input__group .chakra-icon': {
+    opacity: 0.75,
+  },
+}
+
+const FormLabel: ComponentStyleConfig = {
+  baseStyle: () => ({
+    fontSize: 'sm',
+    color: useColorModeValue('gray.600', 'gray.400'), // eslint-disable-line
+  }),
+}
+
+const Input: ComponentStyleConfig = {
+  defaultProps: {
+    size: 'sm',
+  },
+}
+
+const overridedTheme = extendTheme({
   styles: {
-    global: {
-      'html, body': {
-        backgroundColor: 'gray.50',
-      },
-    },
+    global: GlobalStyle,
   },
   config,
+
+  // component overrdes
+  components: {
+    // Icon,
+    FormLabel,
+    Input,
+  },
 })
 
-export default theme
+export default overridedTheme
