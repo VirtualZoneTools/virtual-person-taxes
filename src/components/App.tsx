ea55da7f64e488c795467de9bd3685906a3966c2
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2022-07-21 18:54:20 +0400

    dividend form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,10 @@ import { Container, Icon, IconButton, Stack, VStack } from '@chakra-ui/react'
 import { BiX } from 'react-icons/bi'
 
 import { FormState } from '../utils/state'
-import TaxForm from './TaxForm'
-import Preview from './Preview'
 import Navigation from './Navigation'
+import Dividend from './Dividend'
+import Declaration from './Declaration'
+import DeclarationInstructions from './DeclarationInstructions'
 
 const App: FC = () => {
   const [formState, setFormState] = useState<FormState>()
@@ -38,17 +39,16 @@ const App: FC = () => {
             top={4}
           />
 
-          <Preview data={formState} />
+          <DeclarationInstructions data={formState} />
         </Container>
       ) : (
         <VStack spacing="4" padding={4}>
           <Navigation />
-
           <Routes>
-            <Route path="/dividend" element={<div>test</div>} />
+            <Route path="/dividend" element={<Dividend />} />
             <Route
               path="/declaration"
-              element={<TaxForm data={formState} onSubmit={handleSubmit} />}
+              element={<Declaration data={formState} onSubmit={handleSubmit} />}
             />
             <Route path="/" element={<Navigate to="/declaration" />} />
           </Routes>
